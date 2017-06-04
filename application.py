fa74bc8fd3ef370890d3a2b901c374c2336a0642
VictorKazankov <sadovnichi@mail.ru>
2017-06-04 21:29:29 +0300

    Убрал лишние переменные в Application классе. Убрал лишние отступы в тестовом классе

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ class Application:
 
     def create_group(self, group):
         wd = self.wd
-        self.open_groups_page(wd)
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -30,7 +30,7 @@ class Application:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page(wd)
+        self.return_to_groups_page()
 
     def open_groups_page(self):
         wd = self.wd
@@ -38,7 +38,7 @@ class Application:
 
     def login(self, username, password):
         wd = self.wd
-        self.open_home_page(wd)
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
--- test_add_group.py
+++ test_add_group.py
@@ -9,12 +9,12 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-    def test_add_group(app):
-        app.login(username = "admin", password = "secret")
-        app.create_group(Group(name = "qwert", header = "werty", footer = "yuiop"))
-        app.logout()
+def test_add_group(app):
+    app.login(username = "admin", password = "secret")
+    app.create_group(Group(name = "qwert", header = "werty", footer = "yuiop"))
+    app.logout()
 
-    def test_add_empty_group(app):
-        app.login(username = "admin", password = "secret")
-        app.create_group(Group(name = "", header = "", footer = ""))
-        app.logout()
+def test_add_empty_group(app):
+    app.login(username = "admin", password = "secret")
+    app.create_group(Group(name = "", header = "", footer = ""))
+    app.logout()
