ebef2d9e018ed4d99e73fd6387450421ba494600
VictorKazankov <sadovnichi@mail.ru>
2017-07-22 00:24:08 +0300

    Изменение теста обновления группы

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -9,11 +9,8 @@ def test_update_group_name(app, db, json_groups):
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
     group = random.choice(old_groups)
-    #group_data.id = old_groups.id
+    group_data.id = group.id
     app.group.update_group_by_id(group.id, group_data)
     new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
-    # old_groups.clear()
-    # old_groups.append(group_data)
-    old_groups[group] = group_data
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
