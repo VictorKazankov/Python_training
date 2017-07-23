5c7ffe24310232d0804d61cef25d49506a795546
VictorKazankov <sadovnichi@mail.ru>
2017-07-24 00:40:13 +0300

    Обновление test_update_group.py - 3 версия

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -12,5 +12,7 @@ def test_update_group_name(app, db, json_groups):
     group_data.id = group.id
     app.group.update_group_by_id(group.id, group_data)
     new_groups = db.get_group_list()
+    old_groups.remove(group)
+    old_groups.append(new_groups)
     assert len(old_groups) == len(new_groups)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
